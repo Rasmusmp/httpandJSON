56316aff6759fad92655dbd9072078b0ebd20ff8
Nikolaj H <201205395@post.au.dk>
2015-05-25 18:59:18 +0200

    Implemented:Google Maps

--- app/src/main/java/com/example/rasmus/httpandjson/DetailActivity.java
+++ app/src/main/java/com/example/rasmus/httpandjson/DetailActivity.java
@@ -7,10 +7,24 @@ import android.transition.Slide;
 import android.view.Window;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 
 public class DetailActivity extends Activity {
 
     TextView detailTitle;
+
+
+    static final LatLng HAMBURG = new LatLng(53.558, 9.927);
+    static final LatLng KIEL = new LatLng(53.551, 9.993);
+    private GoogleMap map;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +52,27 @@ public class DetailActivity extends Activity {
         detailTitle.append("\n Dato og tid:          " + date + " : " + time);
         detailTitle.append("\n id:                         " + id);
         detailTitle.append("\n Eventtype:            " + type);
+
+
+        //GOOGLE MAPS
+
+        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
+                .getMap();
+
+        Marker hamburg = map.addMarker(new MarkerOptions().position(HAMBURG)
+                .title("Hamburg"));
+        Marker kiel = map.addMarker(new MarkerOptions()
+                .position(KIEL)
+                .title("Kiel")
+                .snippet("Kiel is cool")
+               );
+
+        // Move the camera instantly to hamburg with a zoom of 15.
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(HAMBURG, 15));
+
+        // Zoom in, animating the camera.
+        map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+
 
     }
 
